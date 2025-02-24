491f53081f40b8a7c958a92ce5f3d4b3246c09ad
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2025-02-24 20:48:46 -0300

    Criando objeto Scanner, e suas respectivas entradas para exibir a mensam final com os dados inseridos pelo usuário.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+            // Criando um objeto Scanner para receber entrada do usuário
+            Scanner scanner = new Scanner(System.in);
+        
+            // Solicitando os dados ao usuário
+            System.out.println("Por favor, digite o número da Conta:");
+            int numero = scanner.nextInt();
+            
+            // Consumir a quebra de linha pendente
+            scanner.nextLine();
+            
+            System.out.println("Por favor, digite o número da Agência:");
+            String agencia = scanner.nextLine();
+            
+            System.out.println("Por favor, digite o seu Nome:");
+            String nomeCliente = scanner.nextLine();
+            
+            System.out.println("Por favor, digite o saldo:");
+            double saldo = scanner.nextDouble();
+            
+            // Fechando o Scanner
+            scanner.close();
+            
+            // Exibindo a mensagem final com os dados do usuário
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                    "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
